537f593e640e8e3bc5fbdd9cd7aae33e956547e9
Lebrugnon <yannick.dahan@hotmail.fr>
2024-12-17 18:03:53 +0100

    First version of Makefile and source.mk for C1m2 Assesment

--- src/sources.mk
+++ src/sources.mk
@@ -1,17 +1,36 @@
 #******************************************************************************
-# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+# Copyright (C) 2024 by Yannick DAHAN
 #
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are 
 # permitted to modify this and use it to learn about the field of embedded
-# software. Alex Fosdick and the University of Colorado are not liable for any
-# misuse of this material. 
+# software. 
 #
 #*****************************************************************************
+ifeq ($(PLATFORM),HOST)
 
 # Add your Source files to this variable
-SOURCES =
+SOURCES = ./main.c \
+		  ./memory.c 
+
+INCLUDES = -I  ~/Dahan_Coursera_m2/include/common \
 
 # Add your include paths to this variable
-INCLUDES = 
 
+
+else ifeq ($(PLATFORM),MSP432)
+SOURCES = ./main.c \
+		  ./memory.c \
+		  ./interrupts_msp432p401r_gcc.c \
+		  ./startup_msp432p401r_gcc.c \
+		  ./system_msp432p401r.c 
+
+INCLUDES = -I  ~/Dahan_Coursera_m2/include/msp432\
+			-I  ~/Dahan_Coursera_m2/include/common\
+			-I  ~/Dahan_Coursera_m2/include/CMSIS
+			 
+
+
+else 
+$(error Platform not supported: $(PLATFORM))
+endif
